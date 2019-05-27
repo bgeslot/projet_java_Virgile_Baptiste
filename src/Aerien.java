e6478452106aa6082b62e97a01c683e8fc7d32a9
bgeslot <baptiste.geslot@gmail.com>
2019-05-27 10:44:39 +0200

    new abstact class Aerien

---
+++ src/Aerien.java
@@ -0,0 +1,34 @@
+public class Aerien extends Vehicule {
+
+    private int heureDeVol;
+
+    //set
+
+    public void setHeureDeVol(int heureDeVol) {
+        this.heureDeVol = heureDeVol;
+    }
+
+    //get
+
+    public int getHeureDeVol() {
+        return heureDeVol;
+    }
+
+    //other method
+
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
