fcd83b48f455996e02e1fb0aa89e687c61a33a87
bgeslot <baptiste.geslot@gmail.com>
2019-05-27 10:41:22 +0200

    new class moto

---
+++ src/Moto.java
@@ -0,0 +1,30 @@
+public class Moto {
+
+    //new bike
+
+    public Moto Moto(){
+        return new Moto();
+    };
+
+
+    //other methode
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
+
+
+}
