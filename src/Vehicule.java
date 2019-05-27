2f8ba78ba8040d3586c0f796e66d0a5666978c3c
bgeslot <baptiste.geslot@gmail.com>
2019-05-27 10:04:15 +0200

    abstact class vehicule

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -1,2 +1,50 @@
-public class Vehicule {
+public abstract class Vehicule {
+
+    private String marque;
+    private String modele;
+    private int vitesseMax;
+    private int prixLocationJour;
+    private String etat;
+
+    public abstract Vehicule Vehicule();
+
+    //set
+
+    public void setMarque(String aMarque){
+        marque=aMarque;
+    }
+    public void setModele(String aModele){
+        modele=aModele;
+    }
+    public void setVitesseMax(int aVitesseMax){
+        vitesseMax=aVitesseMax;
+    }
+    public void setPrixLocationJour(int aPrix){
+        prixLocationJour=aPrix;
+    }
+    public void setEtat(String aEtat){
+        etat=aEtat;
+    }
+
+    // get
+
+    public String getMarque(){
+        return marque;
+    }
+    public String getModele(){
+        return modele;
+    }
+    public int getVitesseMax(){
+        return vitesseMax;
+    }
+    public int getPrixLocationJour(){
+        return prixLocationJour;
+    }
+    public String getEtat(){
+        return etat;
+    }
+
+
+
+
 }
