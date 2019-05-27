1d2c6e9568903f19965220cccda91cbb4d28e8b4
bgeslot <baptiste.geslot@gmail.com>
2019-05-27 12:04:31 +0200

    Menu vehicule

--- src/MenuVehicule.java
+++ src/MenuVehicule.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class MenuVehicule {
 
-    private Vehicule[] listeVehicule;
+    private ArrayList listeVehicule;
 
     public MenuVehicule MenuVrhicule(){
         return new MenuVehicule();
@@ -8,23 +10,54 @@ public class MenuVehicule {
 
     //set
 
-
-    public void setListeVehicule(Vehicule[] listeVehicule) {
+    public void setListeVehicule(ArrayList listeVehicule) {
         this.listeVehicule = listeVehicule;
     }
 
     //get
 
-    public Vehicule[] getListeVehicule() {
+    public ArrayList getListeVehicule() {
         return listeVehicule;
     }
 
     //usefull methode
 
    public void addVehicule(Vehicule aVehicule){
+       ArrayList newListe=new ArrayList();
+       ArrayList oldListe=listeVehicule;
+       int n=oldListe.size();
+       for(int i=0;i<n;++i){
+           newListe.add(i,oldListe.get(i));
+       }
+       newListe.add(n,aVehicule);
 
     }
 
+    public void delVehicule(Vehicule aVehicule){
+        ArrayList listToModif=new ArrayList();
+        if(listToModif.remove(aVehicule)){
+            System.out.println("Element Suprimer");
+        }
+        else{
+            System.out.println("Il n'y a pas de tel vehicule");
+        }
+    }
+
+    // other
 
 
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
