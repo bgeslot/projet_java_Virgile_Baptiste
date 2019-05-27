96ef993f548de66a2dca99c24d79a43b21b42935
bgeslot <baptiste.geslot@gmail.com>
2019-05-27 11:33:53 +0200

    Menu vehicule en cour

---
+++ src/MenuVehicule.java
@@ -0,0 +1,30 @@
+public class MenuVehicule {
+
+    private Vehicule[] listeVehicule;
+
+    public MenuVehicule MenuVrhicule(){
+        return new MenuVehicule();
+    }
+
+    //set
+
+
+    public void setListeVehicule(Vehicule[] listeVehicule) {
+        this.listeVehicule = listeVehicule;
+    }
+
+    //get
+
+    public Vehicule[] getListeVehicule() {
+        return listeVehicule;
+    }
+
+    //usefull methode
+
+   public void addVehicule(Vehicule aVehicule){
+
+    }
+
+
+
+}
