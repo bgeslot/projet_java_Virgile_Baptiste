cf5f316eeceef965d00507b25e527595765dc9c7
Virgile <virgile.petermann@gmail.com>
2019-06-01 22:15:47 +0200

    ALED OU EST MON JDK

---
+++ src/FenetreNouvVehicule.java
@@ -0,0 +1,2 @@
+public class FenetreNouvVehicule {
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    MenuVehicule menuVehicule=new MenuVehicule();
+    MenuClient menuClient=new MenuClient();
+    MenuLocation menuLocation=new MenuLocation();
+    public static void main(String[] args) {
+        private JButton btn nouvClient=new JButton();
+    }
+
+}
