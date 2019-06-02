04ac01168e0bd0c0f7b691772675851bb481a316
Virgile <virgile.petermann@gmail.com>
2019-06-02 21:45:08 +0200

    oui oui c'est bien

--- src/FenetreNouvLocation.java
+++ src/FenetreNouvLocation.java
@@ -1,3 +1,7 @@
-public class FenetreNouvLocation {
+import javax.swing.*;
 
+public class FenetreNouvLocation {
+    private Main main;
+    private JFrame fenetre = new JFrame();
+    private JPanel panel = new JPanel();
 }
