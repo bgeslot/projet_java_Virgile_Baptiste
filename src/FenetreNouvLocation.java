79164892d64243e4d0aa646f1cd8ec388f2fbcd7
Virgile <virgile.petermann@gmail.com>
2019-06-04 15:08:13 +0200

    oui oui c'est bien

--- src/FenetreNouvLocation.java
+++ src/FenetreNouvLocation.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,36 +12,36 @@ public class FenetreNouvLocation implements ActionListener {
     private JPanel panel = new JPanel();
     private JLabel labelPrix = new JLabel();
     private JComboBox jComboBox;
-    private JTextField fieldDateDebut=new JTextField();
-    private JTextField fieldDuree=new JTextField();
-    private JTextField fieldKmPrevi=new JTextField();
-    private JCheckBox boxReduction=new JCheckBox();
+    private JTextField fieldDateDebut = new JTextField();
+    private JTextField fieldDuree = new JTextField();
+    private JTextField fieldKmPrevi = new JTextField();
+    private JCheckBox boxReduction = new JCheckBox();
     private JButton boutonAjouter = new JButton("Ajouter");
     private JButton boutonAnnuler = new JButton("Annuler");
-    private JPanel panel11=new JPanel();
-    private JPanel panel12=new JPanel();
-    private JPanel panel21=new JPanel();
-    private JPanel panel22=new JPanel();
-    private JPanel panel31=new JPanel();
-    private JPanel panel32=new JPanel();
-    private JPanel panel41=new JPanel();
-    private JPanel panel42=new JPanel();
-    private JPanel panel51=new JPanel();
-    private JPanel panel52=new JPanel();
-    private JPanel panel61=new JPanel();
-    private JPanel panel62=new JPanel();
-    private JPanel panel71=new JPanel();
-    private JPanel panel72=new JPanel();
-    private JPanel panel81=new JPanel();
-    private JPanel panel82=new JPanel();
-    private JPanel panel91=new JPanel();
-    private JPanel panel92=new JPanel();
+    private JPanel panel11 = new JPanel();
+    private JPanel panel12 = new JPanel();
+    private JPanel panel21 = new JPanel();
+    private JPanel panel22 = new JPanel();
+    private JPanel panel31 = new JPanel();
+    private JPanel panel32 = new JPanel();
+    private JPanel panel41 = new JPanel();
+    private JPanel panel42 = new JPanel();
+    private JPanel panel51 = new JPanel();
+    private JPanel panel52 = new JPanel();
+    private JPanel panel61 = new JPanel();
+    private JPanel panel62 = new JPanel();
+    private JPanel panel71 = new JPanel();
+    private JPanel panel72 = new JPanel();
+    private JPanel panel81 = new JPanel();
+    private JPanel panel82 = new JPanel();
+    private JPanel panel91 = new JPanel();
+    private JPanel panel92 = new JPanel();
 
-    String[] clients={"Client 0","Client 1","Client 2","Client 3","Client 4","Client 5","Client 6"};
+    String[] clients = {"Client 0", "Client 1", "Client 2", "Client 3", "Client 4", "Client 5", "Client 6"};
 
-    public FenetreNouvLocation(Main main,Vehicule vehicule) {
+    public FenetreNouvLocation(Main main, Vehicule vehicule) {
         this.main = main;
-        this.vehicule=vehicule;
+        this.vehicule = vehicule;
         fenetre.setTitle("Nouvelle location");
         fenetre.setLocationRelativeTo(null);
         fenetre.setVisible(true);
@@ -50,7 +52,18 @@ public class FenetreNouvLocation implements ActionListener {
         fieldKmPrevi.setColumns(15);
         boutonAjouter.addActionListener(this);
         boutonAnnuler.addActionListener(this);
-        panel.setLayout(new GridLayout(9,2));
+        fieldDuree.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                System.out.println("OUIIIIIIIIIII");
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+            }
+        });
+        panel.setLayout(new GridLayout(9, 2));
         panel.add(panel11);
         panel.add(panel12);
         panel.add(panel21);
@@ -70,7 +83,7 @@ public class FenetreNouvLocation implements ActionListener {
         panel.add(panel91);
         panel.add(panel92);
         panel11.add(new JLabel("Location de :"));
-        panel12.add(new JLabel(vehicule.getMarque()+" "+vehicule.getModele()));
+        panel12.add(new JLabel(vehicule.getMarque() + " " + vehicule.getModele()));
         panel21.add(new JLabel("Numéro de Location :"));
         panel22.add(new JLabel(""));
         panel31.add(new JLabel("Client :"));
@@ -94,12 +107,9 @@ public class FenetreNouvLocation implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton recup = (JButton) e.getSource();
-        if (recup==boutonAjouter)
-        {
-
-        }
-        else if (recup==boutonAnnuler)
-        {
+        if (recup == boutonAjouter) {
+            fenetre.dispose();
+        } else if (recup == boutonAnnuler) {
             fenetre.dispose();
         }
     }
