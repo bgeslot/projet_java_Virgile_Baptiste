9cc57be7eddbfc5c73d5bf7c28ed6ec2ec966601
bgeslot <baptiste.geslot@gmail.com>
2019-06-06 18:21:16 +0200

    javadoc

--- src/Aerien.java
+++ src/Aerien.java
@@ -1,12 +1,27 @@
+/**
+ *
+ */
 public class Aerien extends Vehicule {
 
     private String heureDeVol;
 
-    public Aerien()
+    /**
+     *
+     */
+ public Aerien()
     {
 
     }
 
+    /**
+     *
+     * @param marque
+     * @param modele
+     * @param vitesseMax
+     * @param prixLocationJour
+     * @param etat
+     * @param heureDeVol
+     */
     public Aerien(String marque, String modele, String vitesseMax, String prixLocationJour, String etat, String heureDeVol) {
         super(marque, modele, vitesseMax, prixLocationJour, etat);
         this.heureDeVol = heureDeVol;
@@ -15,29 +30,49 @@ public class Aerien extends Vehicule {
 
     //set
 
+    /**
+     *
+     * @param heureDeVol
+     */
     public void setHeureDeVol(String heureDeVol) {
         this.heureDeVol = heureDeVol;
     }
 
     //get
 
+    /**
+     *
+     * @return
+     */
     public String getHeureDeVol() {
         return heureDeVol;
     }
 
     //other method
 
-
+    /**
+     *
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public int hashCode() {
         return super.hashCode();
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return super.toString();
